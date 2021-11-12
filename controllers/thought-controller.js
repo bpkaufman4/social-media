2973adcfa1fd830ef1dba3866ca786ed9d8fd867
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-12 17:01:14 -0600

    created controllers for thoughts

---
+++ controllers/thought-controller.js
@@ -0,0 +1,65 @@
+const { Thought } = require('../models');
+const { db } = require('../models/Thought');
+
+const thoughtController = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    // get one thought by id
+    getThoughtById ({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No Thought found with this ID'})
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // create new Thought
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // update Thought by id
+    updateThought({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true})
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No Thought found with this id' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // delete Thought by id 
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No Thought found with this id '});
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = thoughtController;
